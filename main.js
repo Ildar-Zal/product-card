55c9e514c77dfe8bc86afffcff82a13c8cca7cb8
Ildar Zalyalov <id10997@yandex.ru>
2025-11-04 15:03:10 +0300

    fixed issues

--- main.js
+++ main.js
@@ -1,77 +1,74 @@
-// Покраска всех карточек
+// Покраска всех карточек и сброска цвета
 const productCards = document.querySelectorAll('.product-card');
-const changeColorAllButton = document.querySelector('.button-change-color-all-card');
-const redColorHash = '#FF0000'
+const changeColorAllButton = document.querySelector('#button-change-color-all-card');
 
 changeColorAllButton.addEventListener('click',() => {
-productCards.forEach((card) => card.style.backgroundColor = redColorHash)
+ productCards.forEach((card) => card.classList.toggle('green'));
+ if (changeColorAllButton.innerText == 'Поменять цвет всех карточек') {
+    changeColorAllButton.innerText = 'Сбросить цвет всех карточек';
+  } else {
+    changeColorAllButton.innerText = 'Поменять цвет всех карточек';
+    productCards.forEach((card) => card.classList.remove('red','green'))
+    changeColorFirstCardButton.innerText = 'Поменять цвет первой карточки';
+  }
 })
 
-// Покраска первой карточки
+// Покраска первой карточки и сброска цвета
 
 const firstProductCard = document.querySelector('.product-card');
-const changeColorFirstCardButton = document.querySelector('.button-change-color-first-card');
-const blackColorHash = '#000000';
+const changeColorFirstCardButton = document.getElementById('button-change-color-first-card');
 
 changeColorFirstCardButton.addEventListener('click', () => {
-  firstProductCard.style.backgroundColor = blackColorHash;
-})
-
-// Сброс покраски с карточек
-
-const resetColorAllCardButton = document.querySelector('.button-reset-color-all-card');
-const originalColorHash = '#F7F7FA';
-
-resetColorAllCardButton.addEventListener('click', () => {
-  productCards.forEach((card) => card.style.backgroundColor = originalColorHash);
+  firstProductCard.classList.toggle('red');
+  if (changeColorFirstCardButton.innerText == 'Поменять цвет первой карточки') {
+    changeColorFirstCardButton.innerText = 'Сбросить цвет первой карточки';
+  } else {
+    changeColorFirstCardButton.innerText = 'Поменять цвет первой карточки';
+  }
 })
 
 // Открыть гугл
 
-const openGoogleButton = document.querySelector('.button-open-google');
-
-openGoogleButton.addEventListener ('click', () =>  openGoogle())
+openGoogle();
 
 function openGoogle() {
-  const answer = confirm('Вы действительно хотите открыть Google?')
+  const answer = confirm('Вы действительно хотите открыть Google?');
 
   if (answer==true){
     window.open('https://google.com');
-  }
-  else{
+  } else {
     return;
   }
 }
 
 // Вывод  консоль лог
 
-const outputConsoleLogButton = document.querySelector('.button-output-console-log')
+const outputConsoleLogButton = document.querySelector('#button-output-console-log');
 
-outputConsoleLogButton.addEventListener ('click' , () => outputConsoleLog('ДЗ №4'))
+outputConsoleLogButton.addEventListener('click' , () => outputConsoleLog('ДЗ №4'));
 
 function outputConsoleLog(message) {
-  alert(message)
-  console.log(message)
+  alert(message);
+  console.log(message);
 }
 
 // Вывод консоль лог при наведении на главный заголовок
 
-const mainHeading = document.querySelector('.take-product');
+const mainTitle = document.querySelector('.take-product');
 
-mainHeading.addEventListener ('mouseover', () =>  {
-  console.log(mainHeading)
+mainTitle.addEventListener('mouseover', () => {
+  console.log(mainTitle);
 })
 
 // Смена цвета кнопки
 
-let changeColorForButton = document.querySelector('.button-change-color-for-button');
+const changeColorForButton = document.getElementById('button-change-color-for-button');
 
-changeColorForButton.addEventListener ('click', () => {
+changeColorForButton.addEventListener('click', () => {
 
   if (changeColorForButton.classList.contains('blue')){
-    changeColorForButton.classList.toggle('red')
-  }
-  else{
+    changeColorForButton.classList.toggle('red');
+  } else {
     changeColorForButton.classList.toggle('blue');
   }
   })
