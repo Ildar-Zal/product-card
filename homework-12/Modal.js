a49eec90dfb4bc8bf8bc33f816882833f4e60d01
Ildar Zalyalov <id10997@yandex.ru>
2025-12-21 17:26:59 +0300

    homework-12

---
+++ homework-12/Modal.js
@@ -0,0 +1,46 @@
+export class Modal {
+  constructor(modalId, buttonId, shouldCloseOnOverlay) {
+    this.modal = document.getElementById(modalId);
+    this.overlay = document.getElementById('overlay');
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
+    this.#initOpen(buttonId);
+    this.closeButton = null;
+    this.closeHandler = null
+  }
+
+  open() {
+    this.modal.classList.add('modal-showed');
+    this.overlay.classList.add('overlay-showed');
+    this.#initClose(this.shouldCloseOnOverlay)
+  }
+
+  close() {
+    this.modal.classList.remove('modal-showed');
+    this.overlay.classList.remove('overlay-showed');
+    this.closeButton.removeEventListener('click', this.closeHandler)
+    this.overlay.removeEventListener('click', this.closeHandler)
+  }
+
+  isOpen() {
+    return this.modal.classList.contains('modal-showed');
+  }
+
+  #initOpen(buttonId) {
+    const button = document.getElementById(buttonId);
+    button.addEventListener('click', () => {
+      this.open();
+    })
+  }
+
+  #initClose(shouldCloseOnOverlay) {
+    this.closeHandler = () => {
+      this.close();
+    };
+
+    this.closeButton = this.modal.querySelector('#modal-close-button')
+    this.closeButton.addEventListener('click', this.closeHandler)
+    if (shouldCloseOnOverlay) {
+      this.overlay.addEventListener('click', this.closeHandler)
+    }
+  }
+}
